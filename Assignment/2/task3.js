c35564445fe99fd22dd6b42d4953d32653e8baaa
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2025-04-07 20:43:14 +0530

    Create task3.js

---
+++ Assignment/2/task3.js
@@ -0,0 +1,7 @@
+/*Write a program that checks if a number n is even or odd and
+stores the result in a variable.*/
+
+
+let n = 8907;
+let result = n % 2 === 0 ? "Even" : "Odd";
+console.log(`The number ${n} is ${result}.`);
