227aaafb78dd8e779c33e7c14eb26258bc3e9088
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2025-04-07 20:42:48 +0530

    Create task2.js

---
+++ Assignment/2/task2.js
@@ -0,0 +1,19 @@
+/*Given x = 10 and y = 5, perform the following operations
+and store the results:
+• Addition
+• Multiplication
+• Modulus*/
+
+
+let x = 10;
+let y = 5;
+
+let additionResult = x + y;
+
+let multiplicationResult = x * y;
+
+let modulusResult = x % y;
+
+console.log("Addition Result:", additionResult);
+console.log("Multiplication Result:", multiplicationResult);
+console.log("Modulus Result:", modulusResult);
