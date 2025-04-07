431a259714eee39440bda4c070912ea28d812c02
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2025-04-07 20:43:46 +0530

    Create task5.js

---
+++ Assignment/2/task5.js
@@ -0,0 +1,8 @@
+/*Write a function square that takes a number as input and
+returns its square.*/
+
+function square(number) {
+    return number * number;
+}
+let result = square(4);
+console.log(result);
